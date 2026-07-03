c053ea46d63e3bee2ec63567dedc8a052764e6c6
xiexinyue <1586552582@qq.com>
2026-07-03 17:38:49 +0800

    [cd11]--文本特征抽取TfidfVector

--- day01.py
+++ day01.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_iris;
 from sklearn.model_selection import train_test_split;
 from sklearn.feature_extraction import DictVectorizer;
-from sklearn.feature_extraction.text import CountVectorizer;
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer;
 import jieba;
 
 """
@@ -76,8 +76,31 @@ def count_chinese_demo():
     print('特征名字\n',transfer.get_feature_names())
     return None
 
+"""
+使用TF-idf进行文本特征抽取
+:return:
+"""
+def tf_idf_demo():
+    data=['一种还是一种今天很残酷，明天更残酷，后天很美好，但绝对大部分是死在明天晚上，所以每个人不要放弃今天。',
+          '我们看到的从很远星系来的光是在几百万年前发出的，这样当我们看到宇宙时，我们是在看它的过去。',
+          '如果只用一种方式了解某样事物，你就不会真正了解它。了解实物真正含义的秘密取决于如何将其与我们所了解的事物相联系。'
+          ]
+    data_new = []
+    for d in data:
+        data_new.append(cut_word(d))
+    #1.实例化一个转换器类
+    transfer=TfidfVectorizer()
+    #2.调用fit_transform
+    data_new2=transfer.fit_transform(data_new)
+    print('文本特征抽取\n',data_new2.toarray())
+    print('特征名字\n',transfer.get_feature_names())
+    return None
+
+
+
 if __name__ == '__main__':
     # datasets_demo()
     # dict_demo()
     # count_demo()
-    count_chinese_demo()
+    # count_chinese_demo()
+    tf_idf_demo()
